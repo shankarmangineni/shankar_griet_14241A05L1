baedad515f731bd32c117e8e1857b1e96210dac1
shankarmangineni <31846743+shankarmangineni@users.noreply.github.com>
2017-09-12 14:02:48 +0530

    one

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("HELLO");
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
